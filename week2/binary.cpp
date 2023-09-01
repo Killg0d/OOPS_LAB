7d6f417e67d96ff75939130627fe85a3c284bab8
Tarun Shetty <121545244+Killg0d@users.noreply.github.com>
2023-09-01 09:34:33 +0530

    Update binary.cpp

--- week2/binary.cpp
+++ week2/binary.cpp
@@ -7,21 +7,23 @@ int main() {
 
     int a[32],i,n=decimalNumber;
     
+cout << "Binary representation: "<< endl;
     if (decimalNumber == 0) {
         cout<<"0";
-    } 
+    }
+else {
     for(i=0;n>0;i++)
     {
         a[i]=n%2;
         n/=2;
     }
-    cout << "Binary representation: "<< endl;
+    
     for(i=i-1;i>=0;i--)
     {
         cout<<a[i];
     }
 
-    
+    }
 
     return 0;
 }
