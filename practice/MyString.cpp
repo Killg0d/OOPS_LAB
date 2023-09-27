43c5a001004ddbc5976462b0debb3f76782100ac
Killg0d <shettytarun2002@gmail.com>
2023-09-27 21:20:43 +0530

    MyString Split Function added

--- practice/MyString.cpp
+++ practice/MyString.cpp
@@ -15,6 +15,9 @@ public:
             ;
         return i;
     }
+    void getstring(){
+        cin.getline(ch,sizeof(ch),'\n');
+    }
     void showstring()
     {
         cout << ch;
@@ -29,6 +32,28 @@ public:
         ch[i] = '\0';
     }
     friend istream &operator>>(istream &input, myString &str);
+    friend ostream& operator<<(ostream& output, const myString& str);
+    void split(char delimiter){
+        myString str1[10];
+        int j;
+        int k=0;
+        for(int i=0;ch[i]!='\0';i++)
+        {
+            j=0;
+        
+            while(ch[i]!=delimiter && ch[i]!='\0')
+            {   
+                str1[k].ch[j++]=ch[i++];
+            }
+            str1[k].ch[j]='\0';
+            k++;            
+        }
+        str1[k].ch[0]='\0';
+        for(int i=0;i<k;i++){
+            cout<<str1[i]<<endl;
+        }
+       
+    }
 };
 
 myString strcatsmallest(myString str1, myString str2)
@@ -63,12 +88,24 @@ istream &operator>>(istream &input, myString &str)
     input >> str.ch;
     return input;
 }
+ostream& operator<<(ostream& output, const myString& str) {
+    for (int i = 0; str.ch[i] != '\0'; i++) {
+        output << str.ch[i];
+    }
+    return output;
+}
+
 int main()
 {
     myString str, str1, str2, str3;
-    cin >> str1;
-    str1.showstring();
-    // cin>>str2;
+    //cin >> str1;
+    //cout<<str1;
+    
+    str2.getstring();
+    str2.split(' ');
+    
+
+    //cout<<str3;
     // str3 = strcatsmallest(str1, str2);
     // str3.showstring();
     return 0;
